ac751a5b407da759c9cc658889921e29bc1b8de3
Omkar Shete <omkaruhs@gmail.com>
2024-01-11 05:59:20 +1300

    add react query client

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import AllBlogs from "./pages/AllBlogs.jsx";
 import AddBlog from "./pages/AddBlog.jsx";
 import AuthLayout from "./pages/AuthLayout.jsx";
 import BlogPost from "./pages/BlogPost.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
@@ -73,8 +74,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </Provider>
 );
