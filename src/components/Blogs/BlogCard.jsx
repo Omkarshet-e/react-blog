d1041a084875ec528ed17257b3a5838ab7c28928
Omkar Shete <omkaruhs@gmail.com>
2024-01-10 19:58:20 +1300

    extract blog card into separate component

---
+++ src/components/Blogs/BlogCard.jsx
@@ -0,0 +1,42 @@
+/* eslint-disable react/prop-types */
+import { useNavigate } from "react-router-dom";
+import { FaLongArrowAltRight } from "react-icons/fa";
+function BlogCard({ content }) {
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate("blog")}
+      className="  py-3 px-2 bg-dark-primary-black max-sm:aspect-[10/10] aspect-[10/12] rounded-lg ease-in-out duration-200  flex flex-col justify-between gap-5"
+    >
+      <div>
+        <div className="image w-full p-2">
+          <img
+            src="assets/bg-2.avif"
+            alt="bg-image"
+            className="w-full h-full object-cover border-2 rounded-lg"
+          />
+        </div>
+        <div className="px-2  ">
+          <div className="py-1 font-bold text-xl ">
+            <h1 className="truncate tracking-widest text-primary-pink-variant">
+              Titlejdbfsjhdfbsjdhfnbsdjfnbsdjfsnbdfjshdnfbsd
+            </h1>
+          </div>
+          <div className="py-1 ">
+            <p className="line-clamp-2 leading-5 text-base  font-medium text-primary-pink-variant/75">
+              {content}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pb-4">
+        <button className="text-black font-bold mx-2  bg-primary-pink px-3 py-2 rounded-md flex items-center gap-3">
+          Read More
+          <FaLongArrowAltRight size={20} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
---
+++ src/pages/AllBlogs.jsx
@@ -0,0 +1,61 @@
+import { useEffect } from "react";
+import BlogCard from "../components/Blogs/BlogCard";
+
+function AllBlogs() {
+  useEffect(() => {
+    document.title = "All Blogs";
+    return () => (document.title = "React Blog");
+  }, []);
+  return (
+    <div className="w-full  bg-[#ff758f] min-h-[30vw] py-4 ">
+      <div
+        className="lg:max-w-[calc(88%-4rem)] lg:mx-auto w-full max-lg:px-8 h-full 
+        grid 2xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 "
+      >
+        <BlogCard content={"whjnbfn "} />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb dhjmsnbdf hjndbf jehnbf "
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+        <BlogCard
+          content={
+            "whjnbfn jnfmb ejhfngmbn ejnfm hj,mdnv cdmbf vjdnf vejfnmv ejhdnmb d"
+          }
+        />
+      </div>
+    </div>
+  );
+}
+
+export default AllBlogs;
